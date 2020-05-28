7c8eb75937b11bce6ab2078ef4546cabd52baa60
jeff-h <jeffvh@outlook.com>
2020-05-28 17:04:12 +1000

    add remaining crud for patient controller

--- src/PTFiles.Web/Controllers/PatientsController.cs
+++ src/PTFiles.Web/Controllers/PatientsController.cs
@@ -2,6 +2,9 @@
 using PTFiles.Application.Features.Patients.GetPatients;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using PTFiles.Application.Features.Patients.CreatePatient;
+using PTFiles.Application.Features.Patients.UpdatePatient;
+using PTFiles.Application.Features.Patients.DeletePatient;
 
 namespace PTFiles.Web.Controllers
 {
@@ -18,6 +21,33 @@ namespace PTFiles.Web.Controllers
         public async Task<ActionResult<GetPatientVm>> Get(int id)
         {
             return await Mediator.Send(new GetPatientQuery { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromBody] CreatePatientCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] UpdatePatientCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Mediator.Send(new DeletePatientCommand { Id = id });
+
+            return NoContent();
         }
     }
 }
