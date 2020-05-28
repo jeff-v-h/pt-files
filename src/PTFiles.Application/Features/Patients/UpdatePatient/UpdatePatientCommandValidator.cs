865b1054fdfb056473ca33d149d88a0cef7f167b
jeff-h <jeffvh@outlook.com>
2020-05-28 17:49:51 +1000

    validate update patient command

--- src/PTFiles.Application/Features/Patients/UpdatePatient/UpdatePatientCommandValidator.cs
+++ src/PTFiles.Application/Features/Patients/UpdatePatient/UpdatePatientCommandValidator.cs
@@ -4,37 +4,37 @@ using PTFiles.Application.Common.Interfaces.Persistence;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace PTFiles.Application.Features.Patients.CreatePatient
+namespace PTFiles.Application.Features.Patients.UpdatePatient
 {
-    public class CreatePatientCommandValidator : AbstractValidator<CreatePatientCommand>
+    public class UpdatePatientCommandValidator : AbstractValidator<UpdatePatientCommand>
     {
         private readonly IPTFilesDbContext _context;
 
-        public CreatePatientCommandValidator(IPTFilesDbContext context)
+        public UpdatePatientCommandValidator(IPTFilesDbContext context)
         {
             _context = context;
 
             RuleFor(v => v.FirstName)
                 .NotEmpty().WithMessage("First name is required.")
                 .MaximumLength(50).WithMessage("Title must not exceed 50 characters.");
-            
+
             RuleFor(v => v.LastName)
                 .NotEmpty().WithMessage("Last name is required.")
                 .MaximumLength(50).WithMessage("Last Name must not exceed 50 characters.");
 
             RuleFor(v => v.DOB)
-                .NotEmpty().WithMessage("DOB is required.");           
-           
+                .NotEmpty().WithMessage("DOB is required.");
+
             RuleFor(v => v.Email)
                 .MaximumLength(255).WithMessage("Email must not exceed 255 characters.")
                 .MustAsync(BeUniqueEmailIfExists).WithMessage("The specified email already exists.");
-             
+
             RuleFor(v => v.HomePhone)
-                .MaximumLength(40).WithMessage("Home phone must not exceed 40 characters.");            
-            
+                .MaximumLength(40).WithMessage("Home phone must not exceed 40 characters.");
+
             RuleFor(v => v.MobilePhone)
                 .MaximumLength(40).WithMessage("Mobile phone must not exceed 40 characters.");
-            
+
             RuleFor(v => v.Occupation)
                 .MaximumLength(50).WithMessage("Occupation must not exceed 50 characters.");
         }
