e2d211d9e4295d332ba96337e0058976408ac94a
jeff-h <jeffvh@outlook.com>
2020-05-29 21:15:12 +1000

    update routes

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,22 +1,26 @@
-import * as React from "react";
-import { Switch, Route } from "react-router-dom";
-import Dashboard from "./components/dashboard/Dashboard";
-import Patient from "./components/patient/Patient";
-import CaseFile from "./components/caseFile/CaseFile";
-import Consultation from "./components/consultation/Consultation";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import PatientPage from './components/patient/PatientPage';
+import CasefilesPage from './components/casefiles/CasefilesPage';
+import ConsultationPage from './components/consultation/ConsultationPage';
+import ConsultationsPage from './components/consultations/ConsultationsPage';
+import PatientsPage from './components/patients/PatientsPage';
+import CasefilePage from './components/casefile/CasefilePage';
 
 export default class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route path="/patients/:id" component={Patient as any} />
-        <Route path="/patients" component={Dashboard} />
-        <Route path="/casefiles/:id" component={CaseFile as any} />
         <Route
-          path="/consultations/:consultId"
-          component={Consultation as any}
+          path="/patients/:patientId/casefiles/:casefileId/consultations/:consultId"
+          component={ConsultationPage}
         />
+        <Route path="/patients/:patientId/casefiles/:casefileId/consultations" component={ConsultationsPage} />
+        <Route path="/patients/:patientId/casefiles/:casefileId" component={CasefilePage} />
+        <Route path="/patients/:patientId/casefiles" component={CasefilesPage} />
+        <Route path="/patients/:patientId" component={PatientPage} />
+        <Route exact path={['/', '/patients']} component={PatientsPage} />
       </Switch>
     );
   }
